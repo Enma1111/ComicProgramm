5e6647456d6990c6f910ad087248c25d8161d544
Long_John <133089195+DaiouEnma@users.noreply.github.com>
2024-03-29 11:26:21 +0100

    zweiter push

---
+++ src/main/java/jan/bartalsky/comic/Service/OptionController.java
@@ -0,0 +1,28 @@
+package jan.bartalsky.comic.Service;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class OptionController {
+
+    @FXML
+    private Button BtnMovie;
+    @FXML
+    private Button BtnComic;
+    @FXML
+    private Button BtnBook;
+
+    @FXML
+    public void EnterComicPnl(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void EnterBookPnl(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void EnterMoviePnl(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module jan.bartalsky.comic {
 
     opens jan.bartalsky.comic to javafx.fxml;
     exports jan.bartalsky.comic;
+    exports jan.bartalsky.comic.Service;
+    opens jan.bartalsky.comic.Service to javafx.fxml;
 }
