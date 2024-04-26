5bb48dd500319de8cc6d528f76e2a2408292c030
Long_John <133089195+DaiouEnma@users.noreply.github.com>
2024-04-26 09:18:53 +0200

    Speichern funktioniert wieder

--- src/main/java/jan/comic/Helper/PreparedStatementHelper.java
+++ src/main/java/jan/comic/Helper/PreparedStatementHelper.java
@@ -10,12 +10,11 @@ public class PreparedStatementHelper {
 
     public void insertPreparedStatement(PreparedStatement preparedStatement, String @NotNull [] values) throws SQLException {
         for (int i = 0; i < values.length; i++) {
-            preparedStatement.setString(i, values[i]);
+            preparedStatement.setString(i + 1, values[i]);
         }
         preparedStatement.addBatch();
 
         preparedStatement.executeBatch();
-
     }
 
     public void deletePreparedStatement(@NotNull PreparedStatement preparedStatement, String id) throws SQLException {
